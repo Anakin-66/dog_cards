5f20283a209d5bf01861783fabc45ddcb53307ee
Frangeul Nathan <seekerz17@outlook.fr>
2023-11-06 12:29:23 +0100

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const cards = document.querySelectorAll(".card");
     card.addEventListener("click", () => {
         // Je répète la prodécure de la ligne 1 sauf que j'utilise un querySelector basique
       const availability = card.querySelector(".availability");
-        // Je change le display de base qui est en "none" à "block" pour pouvoir l'afficher à mon clique
-      availability.style.display = "block"
+        // Je change le display de base qui est en "none" à "block" pour pouvoir l'afficher à mon clique, je déclare ensuite un boolean pour avoir un toggle on et off.
+        // Je lui dis que si le style.display de ma variable availability est strictement égal à none alors il change mon display en block, sinon il me le remet en none
+        if (availability.style.display === "none") {
+            availability.style.display = "block";
+          } else {
+            availability.style.display = "none";
+          }
     });
   });
